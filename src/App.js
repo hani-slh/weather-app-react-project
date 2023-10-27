65ce06a51a37e29e3c1ab9dd6cfa5f6a8bd806db
hani-slh <140309588+hani-slh@users.noreply.github.com>
2023-10-27 15:50:06 +0330

    added html and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          Open-source code by{" "}
+          <a href="https://github.com/hani-slh/weather-app-react-project">
+            Haniye Salehi
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
